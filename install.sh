141f4106822435a797dc899fb3de6c14a63c065b
Pratyush ojha <pratyush_ojha@epam.com>
2026-03-11 11:43:59 +0530

    updated install

--- install.sh
+++ install.sh
@@ -7,20 +7,23 @@ echo "Installing BashForge..."
 INSTALL_DIR="$HOME/.bashforge"
 BIN_DIR="/usr/local/bin"
 
-# Create directory
 mkdir -p $INSTALL_DIR
 
-# Download latest script
-curl -L https://raw.githubusercontent.com/pratyushojha-coder/bashforge/main/bashforge.py -o $INSTALL_DIR/bashforge.py
+curl -L https://raw.githubusercontent.com/pratyushojha-coder/bashforge/main/bashforge.py \
+-o $INSTALL_DIR/bashforge.py
 
-# Make executable
 chmod +x $INSTALL_DIR/bashforge.py
 
-# Create global command
-sudo ln -sf $INSTALL_DIR/bashforge.py $BIN_DIR/bashforge
+# Create launcher
+sudo tee $BIN_DIR/bashforge > /dev/null <<EOF
+#!/bin/bash
+python3 $INSTALL_DIR/bashforge.py
+EOF
+
+sudo chmod +x $BIN_DIR/bashforge
 
 echo ""
 echo "✅ BashForge installed successfully!"
-echo "Run it using:"
 echo ""
+echo "Run:"
 echo "bashforge"
